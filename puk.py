126393d4cd19d09af0eef0d65988eef4fb11342f
poli <polishka3004@gmail.com>
2022-10-05 03:14:33 +0300

    fixed work with userstates

--- puk.py
+++ puk.py
@@ -9,8 +9,7 @@ logging.basicConfig(level=logging.INFO)
 token = ""
 bot = Bot(token=token)
 
-dp = Dispatcher(bot)
-storage = MemoryStorage()
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 
 class userstate(StatesGroup):
@@ -18,23 +17,23 @@ class userstate(StatesGroup):
     decode = State()
 
 
-@dp.message_handler(commands=['decode'])
+@dp.message_handler(state='*', commands=['decode'])
 async def decode_command(message: types.Message):
     await message.reply("type awawawa to decode")
     await userstate.decode.set()
 
 
-@dp.message_handler(state=userstate.decode)
-async def decode(message: types.Message):
-    await message.reply("".join(chr(int(i.replace('a', '0')
-                                        .replace('w', '1'), 2)) for i in
-                                message.text.split()))
-
-
-@dp.message_handler(commands=['encode'])
+@dp.message_handler(state='*', commands=['encode'])
 async def encode_command(message: types.Message):
     await message.reply("type awawawa to encode")
     await userstate.encode.set()
+
+
+@dp.message_handler(state=userstate.decode)
+async def decode(message: types.Message, state: FSMContext):
+    await message.reply("".join(chr(int(i.replace('a', '0')
+                                        .replace('w', '1'), 2)) for i in
+                                message.text.split()))
 
 
 @dp.message_handler(state=userstate.encode)
